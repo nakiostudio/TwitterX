605b4d451596be389a6a3fa963ea3e590c67977a
Carlos Vidal <nakioparkour@gmail.com>
2018-02-20 21:25:39 +0000

    rounded tweet composer and profile avatar views

--- TwitterX/TwitterX/Features/TWXRoundedAvatars.m
+++ TwitterX/TwitterX/Features/TWXRoundedAvatars.m
@@ -4,10 +4,18 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@interface NSView (TWX)
+
+- (void)roundView;
+
+@end
+
 @implementation TWXRoundedAvatars
 
 + (void)loadFeature {
     [TWXRuntime exchangeInstanceMethod:@"layout" ofClass:@"TMAvatarImageView"];
+    [TWXRuntime exchangeInstanceMethod:@"layout" ofClass:@"TMEditableAvatarView"];
+    [TWXRuntime exchangeInstanceMethod:@"layout" ofClass:@"TMComposerAvatarView"];
     [TWXRuntime exchangeInstanceMethod:@"setBaseImage:" ofClass:@"TMAvatarSidebarButton"];
 }
 
@@ -19,15 +27,29 @@ NS_ASSUME_NONNULL_BEGIN
     [self TMAvatarImageView_layout];
     
     if ([self isKindOfClass:[@"TMAvatarImageView" twx_class]]) {
-        NSView *const view = (NSView *)self;
-        view.wantsLayer = YES;
-        view.layer.masksToBounds = YES;
-        view.layer.cornerRadius = view.bounds.size.height * 0.5f;
+        [(NSView *)self roundView];
+    }
+}
+
+- (void)TMEditableAvatarView_layout {
+    [self TMEditableAvatarView_layout];
+    
+    if ([self isKindOfClass:[@"TMEditableAvatarView" twx_class]]) {
+        [(NSView *)self roundView];
+        [((NSView *)self).superview roundView];
+    }
+}
+
+- (void)TMComposerAvatarView_layout {
+    [self TMComposerAvatarView_layout];
+    
+    if ([self isKindOfClass:[@"TMComposerAvatarView" twx_class]]) {
+        [(NSView *)self roundView];
     }
 }
 
 - (void)TMAvatarSidebarButton_setBaseImage:(nullable NSImage *)image {
-    if (image == nil) {
+    if (image == nil || ![self isKindOfClass:[@"TMAvatarSidebarButton" twx_class]]) {
         [self TMAvatarSidebarButton_setBaseImage:image];
         return;
     }
@@ -35,12 +57,22 @@ NS_ASSUME_NONNULL_BEGIN
     NSImage *const roundedImage = [[NSImage alloc] initWithSize:[image size]];
     [roundedImage lockFocus];
     [[NSGraphicsContext currentContext] setImageInterpolation:NSImageInterpolationHigh];
-    NSRect imageFrame = NSRectFromCGRect(CGRectMake(0, 0, image.size.width, image.size.height));
+    NSRect const imageFrame = NSRectFromCGRect(CGRectMake(0, 0, image.size.width, image.size.height));
     [[NSBezierPath bezierPathWithRoundedRect:imageFrame xRadius:image.size.height * 0.5f yRadius:image.size.height * 0.5f] addClip];
     [image drawAtPoint:NSZeroPoint fromRect:NSMakeRect(0, 0, image.size.width, image.size.height) operation:NSCompositingOperationSourceOver fraction:1];
     [roundedImage unlockFocus];
     
     [self TMAvatarSidebarButton_setBaseImage:roundedImage];
+}
+
+@end
+
+@implementation NSView (TWX)
+
+- (void)roundView {
+    self.wantsLayer = YES;
+    self.layer.masksToBounds = YES;
+    self.layer.cornerRadius = self.bounds.size.height * 0.5f;
 }
 
 @end
