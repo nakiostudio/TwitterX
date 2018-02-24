9d45b2091c77d0de27a29d5e42e796ee0cefa768
Carlos Vidal <nakioparkour@gmail.com>
2018-02-24 15:50:45 +0000

    fixed tweet texfield with wrong background color bug

--- TwitterX/TwitterX/Features/TWXRoundedStatusContent.m
+++ TwitterX/TwitterX/Features/TWXRoundedStatusContent.m
@@ -18,8 +18,11 @@ NS_ASSUME_NONNULL_BEGIN
     [self TMNewTimelineStatusCell_layout];
     
     if ([self isKindOfClass:[@"TMNewTimelineStatusCell" twx_class]]) {
+        NSView *const cell = (NSView *)self;
         NSView *const containerView = (NSView *)[self performSelector:@selector(containerView)];
         containerView.layer.cornerRadius = 12.0f;
+        NSTextField *const textField = (NSTextField *)[self performSelector:@selector(tweetTextField)];
+        textField.backgroundColor = [NSColor colorWithCGColor:cell.layer.backgroundColor];
     }
 }
 
